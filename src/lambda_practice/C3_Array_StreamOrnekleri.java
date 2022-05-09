a3e8a0c6b377c5a62bcf31cf92bdbe184affd3bb
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-09 19:57:05 +0300

    09.05.22

--- src/lambda_practice/C3_Array_StreamOrnekleri.java
+++ src/lambda_practice/C3_Array_StreamOrnekleri.java
@@ -15,19 +15,36 @@ public class C3_Array_StreamOrnekleri {
         // kullanabilmek icin donusum yapilmalidir
 
         Integer arr1 [] ={1,5,-5,6,12,-8,9,3,4};
+        int [] arr2 = {1,5,-5,6,12,-8,9,3,4};
 
+        System.out.println("listele(arr1) = " + listele(arr1));
+        System.out.println("toplam(arr1) = " + toplam(arr1));
+        System.out.println("toplam2Sum(arr2) = " + toplam2Sum(arr2));
+        ortalama(arr2);
 
     }
     //S1: arrayin elemanlarini bir liste yazdiralim
-
+        public static List<Integer> listele(Integer [] arr){
+        return Arrays.stream(arr).collect(Collectors.toList());
+        }
 
 
     //S2: Arrayin elemanlarini toplamini bulalim
 
+    public static int toplam (Integer [] arr){
+        Stream<Integer> arrYeni =Stream.of(arr);
+        return arrYeni.reduce(0,(x,y)-> x+y);
+    }
 
+    public static int toplam2Sum (int [] arr){
+        return Arrays.stream(arr).sum();
+    }
 
     // S3: Array in elemanlarinin ortalamasini bulalim yazdir
 
+    public static void ortalama (int [] arr){
+       Arrays.stream(arr).average().ifPresent(t->System.out.println(t));
+    }
 
 
 
